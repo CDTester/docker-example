89b35b9719184a6942d24f736df9524040f52c6c
CDTester <chris_a_day@hotmail.com>
2026-02-28 14:24:03 +0000

    github actions

--- playwright/entrypoint.sh
+++ playwright/entrypoint.sh
@@ -2,10 +2,11 @@
 set -e
 
 # Run Playwright tests (will still continue even if tests fail)
-npm run testChrome || true
+npm run testChrome -- $TEST_ARGS|| true
 
 # Generate Allure report
 npx allure generate allure-results --clean -o allure-report
+echo "Report generated at /app/allure-report"
 
 # Bind to 0.0.0.0 so it's accessible outside the container
-npx allure open allure-report --port 4040 --host 0.0.0.0
+# npx allure open allure-report --port 4040 --host 0.0.0.0
